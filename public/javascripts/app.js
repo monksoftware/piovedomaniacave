1100f8bfc8740fde9793504ebd0d15cb0e48e77e
Andrea D'Ippolito <adedip@gmail.com>
2014-04-11 17:14:45 +0200

    background icons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,7 @@ angular.module('app', []);
 
 angular.module('app').controller('AppCtrl', function($scope, $http) {
   $scope.forecast = "";
+  $scope.icon = "sole";
   $scope.element = document.getElementsByTagName('h1')[0];
   $scope.weatherData = $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Cave&mode=json&units=metric&cnt=1&APPID=e21092a5856008cea4bc2ab452ab154a');
   $scope.resize = function() {
@@ -10,7 +11,13 @@ angular.module('app').controller('AppCtrl', function($scope, $http) {
 
   $scope.weatherData.then(function(data) {
     var rainStatus = [500,501,502,503,504,511,520,521,522,531];
-    $scope.forecast = rainStatus.indexOf(data.data.list[0].weather[0].id) != -1 ? "SI" : "NO";
+    if(rainStatus.indexOf(data.data.list[0].weather[0].id) != -1){
+      $scope.forecast = "SI";
+      $scope.icon = "pioggia";
+    }else{
+      $scope.forecast = "NO";
+      $scope.icon = "sole";
+    } 
     $scope.resize();
   });
 
